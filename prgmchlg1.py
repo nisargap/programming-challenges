356fe4eb191612a657cbc3d1ffb2dd0a4bce64ad
Nisarga <patelnisarga1@gmail.com>
2015-06-04 17:36:46 -0400

    changes

--- prgmchlg1.py
+++ prgmchlg1.py
@@ -1,7 +1,5 @@
 from math import factorial
 def results(a,b):
-	if abs(a-b) > 1:
-		return 0
 	if a < 25 and b < 25:
 		return factorial(a+b)/(factorial(b) * factorial(a))
 	elif a == 25:
@@ -11,4 +9,3 @@ def results(a,b):
 	elif a == b + 2 and b > 23:
 		return (factorial(48)/(factorial(24)* factorial(48 - 24))) * (2 ** b - 24)
 print(results(3,25))
-print(results(24,17))
