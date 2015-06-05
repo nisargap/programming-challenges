08aeb83ab75cc2724b317718821cfcb7132ea5ea
Nisarga <patelnisarga1@gmail.com>
2015-06-04 21:28:39 -0400

    Programming Challenge

--- prgmchlg1.py
+++ prgmchlg1.py
@@ -8,8 +8,15 @@ def results(a,b):
 	elif b == 25:
 		return  factorial(24+a)/(factorial(a) * factorial(24))
 	elif a == b + 2 and b > 23:
-		return (factorial(48)/(factorial(24)* factorial(48 - 24))) * (2 ** b - 24)
-	elif b == a + 2 and a > 23
-		return (factorial(48)/(factorial(24)* factorial(48 - 24))) * (2 ** a - 24)
+		return (factorial(48)/(factorial(24)* factorial(48 - 24))) * (2 ** b - 24) % (10 ** 9)
+	elif b == a + 2 and a > 23:
+		return (factorial(48)/(factorial(24)* factorial(48 - 24))) * (2 ** a - 24) % (10 ** 9)
+
+# test case 1
 print(results(3,25))
+
+# test case 2
 print(results(24,17))
+
+# test case 3
+print(results(10**9, (10**9)-2))
